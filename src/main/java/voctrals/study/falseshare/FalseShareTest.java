72f2cd17e6a3909100b14110e453aa8298e87bf5
liulei <lei.liu@17zuoye.com>
2019-08-06 15:38:57 +0800

    伪共享...

---
+++ src/main/java/voctrals/study/falseshare/FalseShareTest.java
@@ -0,0 +1,63 @@
+package voctrals.study.falseshare;
+
+/**
+ * @author lei.liu
+ * @since 19-8-6
+ */
+public class FalseShareTest implements Runnable {
+
+    public static int NUM_THREADS = 4;
+    public final static long ITERATIONS = 1000L * 1000L;
+    private final int arrayIndex;
+    private static VolatileLong[] longs;
+    public static long SUM_TIME = 0L;
+
+    public FalseShareTest(final int arrayIndex) {
+        this.arrayIndex = arrayIndex;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        Thread.sleep(10000);
+        for (int j = 0; j < 10; j++) {
+            System.out.println(j);
+            if (args.length == 1) {
+                NUM_THREADS = Integer.parseInt(args[0]);
+            }
+            longs = new VolatileLong[NUM_THREADS];
+            for (int i = 0; i < longs.length; i++) {
+                longs[i] = new VolatileLong();
+            }
+            final long start = System.nanoTime();
+            runTest();
+            final long end = System.nanoTime();
+            SUM_TIME += end - start;
+        }
+        System.out.println("平均耗时：" + SUM_TIME / 10);
+    }
+
+    private static void runTest() throws InterruptedException {
+        Thread[] threads = new Thread[NUM_THREADS];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new FalseShareTest(i));
+        }
+        for (Thread t : threads) {
+            t.start();
+        }
+        for (Thread t : threads) {
+            t.join();
+        }
+    }
+
+    public void run() {
+        long i = ITERATIONS + 1;
+        while (0 != --i) {
+            longs[arrayIndex].value = i;
+        }
+    }
+
+    public final static class VolatileLong {
+        public volatile long value = 0L;
+        // 这一行的作用是让VolatileLong对象独享一个缓存行，导致缓存行不会被多个处理器处理
+        public long p1, p2, p3, p4, p5, p6;     //屏蔽此行
+    }
+}
